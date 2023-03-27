559b5284e51efc07650978c5843ca67db78a6296
IIMKGII <waigmann.thorsten@gmail.com>
2023-03-27 23:14:50 +0200

    a lot of BS

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,59 +5,61 @@ using System.Security.Cryptography;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
     public TMP_Dropdown imageDropdown;
     public Image mainCanvasImage;
-    
+    public Image mainCalloutImage;
+
     private GameObject currentDraggableImage;
 
-    public Sprite[] imageSprites;
+    [FormerlySerializedAs("imageSprites")] public Sprite[] normalMaps;
+    public Sprite[] calloutMaps;
+    
     
     public GameObject[] draggableImages;
 
-    private bool isSelected, smallDrag, deleteImg;
+    private bool isSelected, smallDrag, deleteImg, drawingEnabled;
 
     private int currentIndex;
     
     private float timer;
 
+    public Button colorButton;
+
+    public Color currentColor;
+
     void Start()
     {
         // Populate the dropdown options
         imageDropdown.ClearOptions();
-        foreach (Sprite sprite in imageSprites)
+        foreach (Sprite sprite in normalMaps)
         {
             imageDropdown.options.Add(new TMP_Dropdown.OptionData(sprite.name));
         }
         imageDropdown.value = 0;
         imageDropdown.RefreshShownValue();
+        
+        currentColor = new Color(0.00392156862745098f, 0.42745098039215684f, 0.5019607843137255f);
+        setupColors();
     }
 
     public void OnDropdownValueChanged(int value)
     {
         // Set the sprite on the main canvas image component
-        mainCanvasImage.sprite = imageSprites[value];
+        mainCanvasImage.sprite = normalMaps[value];
         mainCanvasImage.preserveAspect = true;
+        mainCalloutImage.sprite = calloutMaps[value];
+        mainCalloutImage.preserveAspect = true;
     }
-
+    
 
     private void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Delete))
-        {
-            Destroy(currentDraggableImage);
-        }
-
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            timer = 0f;
-            smallDrag = false;
-        }
+        checkNumbers();
         
         if (smallDrag)
         {
@@ -65,19 +67,11 @@ public class Controller : MonoBehaviour
         }
         
         
-        if (isSelected && Input.GetMouseButtonDown(0))
-        {
-            currentDraggableImage = Instantiate(draggableImages[currentIndex], Input.mousePosition, Quaternion.identity);
-            currentDraggableImage.transform.SetParent(FindObjectOfType<ImageEditor>().transform);
-            Destroy(currentDraggableImage.GetComponent<Button>());
-            currentDraggableImage.tag = "button";
-            float scale = FindObjectOfType<ImageEditor>().canvasScale;
-            currentDraggableImage.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(100 * scale, 100 * scale);
-        }
 
         bool foundSth = false;
+        bool spawn = true;
         
-        if (EventSystem.current.IsPointerOverGameObject())
+        if (EventSystem.current.IsPointerOverGameObject() && !drawingEnabled)
         {
             PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
             pointerEventData.position = Input.mousePosition;
@@ -86,6 +80,11 @@ public class Controller : MonoBehaviour
 
             foreach (RaycastResult result in results)
             {
+                if (result.gameObject.CompareTag("UIElement"))
+                {
+                    spawn = false;
+                    break;
+                }
                 if (result.gameObject.CompareTag("button") && !smallDrag)
                 {
                     foundSth = true;
@@ -106,23 +105,156 @@ public class Controller : MonoBehaviour
                 }
             }
         }
+        
+        if (isSelected && Input.GetMouseButtonDown(0) && !drawingEnabled && spawn)
+        {
+            currentDraggableImage = Instantiate(draggableImages[currentIndex], Input.mousePosition, Quaternion.identity);
+            currentDraggableImage.transform.SetParent(FindObjectOfType<ImageEditor>().transform);
+            Destroy(currentDraggableImage.GetComponent<Button>());
+            currentDraggableImage.tag = "button";
+            currentDraggableImage.GetComponent<Image>().color = currentColor;
+            float scale = FindObjectOfType<ImageEditor>().canvasScale;
+            currentDraggableImage.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(50 * scale, 50 * scale);
+        }
 
         isSelected = !foundSth;
+    }
+
+    private void checkNumbers()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentColor = new Color(0.00392156862745098f, 0.42745098039215684f, 0.5019607843137255f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentColor = currentColor = new Color(0.5137254902f, 0.0745098039f, 0.6196078431f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentColor = new Color(0.0823529412f, 0.8078431373f, 0.8901960784f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currentColor = new Color(0.6549019608f, 0.0470588235f, 0.2352941176f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            currentColor = new Color(0.0392156863f, 0.7019607843f, 0.3490196078f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            currentColor = new Color(0.9333333333f, 0.9294117647f, 0.2078431373f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            currentColor = Color.black;
+            currentColor.a = 0;
+        }
+        
+        setupColors();
+        
+        
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            Destroy(currentDraggableImage);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            timer = 0f;
+            smallDrag = false;
+        }
     }
 
     public void currentSelected(int i)
     {
         deleteImg = false;
+        drawingEnabled = false;
         currentIndex = i;
-        isSelected = true; 
-        Destroy(currentDraggableImage);
+        isSelected = true;
     }
 
+    public void rotateObject(float number)
+    {
+        currentDraggableImage.transform.rotation = Quaternion.Euler(0, 0, number*360);
+    }
+
+
+    public void toggleMaps(bool isNormal)
+    {
+        mainCalloutImage.gameObject.SetActive(!isNormal);
+    }
+    
     public void exportPDF()
     {
         isSelected = false;
     }
+
+    public void drawMode()
+    {
+        isSelected = false;
+        drawingEnabled = true;
+    }
     
-    
-    
+    public bool getDrawingEnabled()
+    {
+        return drawingEnabled;
+    }
+
+    public void colorSelector(int i)
+    {
+        switch(i)
+        {
+            case 0:
+                currentColor = new Color(0.00392156862745098f, 0.42745098039215684f, 0.5019607843137255f);
+
+                break;
+            case 1:
+                currentColor = currentColor = new Color(0.5137254902f, 0.0745098039f, 0.6196078431f);
+
+                break;
+            case 2:
+                currentColor = new Color(0.0823529412f, 0.8078431373f, 0.8901960784f);
+                break;
+            case 3:
+                currentColor = new Color(0.6549019608f, 0.0470588235f, 0.2352941176f);
+
+                break;
+            case 4:
+                currentColor = new Color(0.0392156863f, 0.7019607843f, 0.3490196078f);
+                break;
+            case 5: 
+                currentColor = new Color(0.9333333333f, 0.9294117647f, 0.2078431373f);
+                break;
+            case 6: 
+                currentColor = Color.black;
+                currentColor.a = 0;
+                break;
+                
+            
+        }
+
+        setupColors();
+    }
+
+
+    private void setupColors()
+    {
+        ColorBlock colorBlock = new ColorBlock
+        {
+            normalColor = currentColor,
+            highlightedColor = currentColor,
+            pressedColor = currentColor,
+            disabledColor = currentColor,
+            colorMultiplier = 1,
+            fadeDuration = 0.1f
+        };
+
+        colorButton.colors = colorBlock;
+
+        foreach (var image in draggableImages)
+        {
+            image.GetComponent<Button>().colors = colorBlock;
+        }
+    }
 }
--- Assets/Scripts/ImageEditor.cs
+++ Assets/Scripts/ImageEditor.cs
@@ -1,24 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ImageEditor : MonoBehaviour
 {
-    public float zoomSpeed =1f;
+    public float zoomSpeed = 1f;
     public float panSpeed = 1f;
 
     private Vector3 canvasPosition;
     private Vector3 lastMousePosition;
+
     public float canvasScale;
+
+    // private LineRenderer lineRenderer;
+    public Image imageComponent;
+
+
+    private Texture2D texture;
+    private Color[] colors;
+    private Vector2 previousPosition;
+    private bool isDrawing;
+
+
 
     void Start()
     {
         canvasPosition = transform.localPosition;
         canvasScale = transform.localScale.x;
+
+
+        texture = new Texture2D((int)imageComponent.rectTransform.rect.width,
+            (int)imageComponent.rectTransform.rect.height);
+        colors = texture.GetPixels();
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = Color.black;
+            colors[i].a = 0;
+        }
+
+        texture.SetPixels(colors);
+        texture.Apply();
+        imageComponent.sprite =
+            Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one * 0.5f);
+
     }
 
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            //save screenshot
+        }
+
+        if (Input.GetMouseButtonDown(0) && FindObjectOfType<Controller>().getDrawingEnabled())
+        {
+            isDrawing = true;
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(imageComponent.rectTransform, Input.mousePosition,
+                null, out localPoint);
+            previousPosition = localPoint;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDrawing = false;
+        }
+
+        if (isDrawing)
+        {
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(imageComponent.rectTransform, Input.mousePosition,
+                null, out localPoint);
+            int x1 = Mathf.RoundToInt(previousPosition.x + imageComponent.rectTransform.rect.width / 2);
+            int y1 = Mathf.RoundToInt(previousPosition.y + imageComponent.rectTransform.rect.height / 2);
+            int x2 = Mathf.RoundToInt(localPoint.x + imageComponent.rectTransform.rect.width / 2);
+            int y2 = Mathf.RoundToInt(localPoint.y + imageComponent.rectTransform.rect.height / 2);
+
+            Color currentColor = FindObjectOfType<Controller>().currentColor;
+
+            if (currentColor.a == 0)
+            {
+                DrawLine(x1, y1, x2, y2, currentColor, 20);
+            }
+            else
+            {
+                DrawLine(x1, y1, x2, y2, currentColor, 8);
+            }
+            previousPosition = localPoint;
+        }
+
+
+
         // Zoom with scroll wheel
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         canvasScale += scroll * zoomSpeed;
@@ -29,6 +104,7 @@ public class ImageEditor : MonoBehaviour
         {
             lastMousePosition = Input.mousePosition;
         }
+
         if (Input.GetMouseButton(2))
         {
             Vector3 delta = Input.mousePosition - lastMousePosition;
@@ -40,5 +116,54 @@ public class ImageEditor : MonoBehaviour
         // Update canvas transform
         transform.localScale = Vector3.one * canvasScale;
         transform.localPosition = canvasPosition;
+    }
+
+    public void ClearDrawing()
+    {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 0;
+    }
+
+    void DrawLine(int x1, int y1, int x2, int y2, Color color, int size)
+    {
+
+        x1 = x1 - size / 2;
+        y1 = y1 - size / 2;
+        x2 = x2 - size / 2;
+        y2 = y2 - size / 2;
+        
+        int dx = Mathf.Abs(x2 - x1);
+        int dy = Mathf.Abs(y2 - y1);
+        int sx = (x1 < x2) ? 1 : -1;
+        int sy = (y1 < y2) ? 1 : -1;
+        int err = dx - dy;
+        
+        while (true)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int pixelIndex = (y1 + i) * texture.width + (x1 + j);
+                    colors[pixelIndex] = color;
+                }
+            }
+        
+            if (x1 == x2 && y1 == y2) break;
+            int e2 = 2 * err;
+            if (e2 > -dy)
+            {
+                err -= dy;
+                x1 += sx;
+            }
+            if (e2 < dx)
+            {
+                err += dx;
+                y1 += sy;
+            }
+        }
+        
+        texture.SetPixels(colors);
+        texture.Apply();
     }
 }
